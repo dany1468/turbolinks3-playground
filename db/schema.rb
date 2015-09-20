7017bf9b38679baeb74e4486f05327c12741ac93
dany1468 <dany1468@gmail.com>
2015-09-21 00:51:21 +0900

    Add Book model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520154531) do
+ActiveRecord::Schema.define(version: 20150906145309) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.string   "author",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
